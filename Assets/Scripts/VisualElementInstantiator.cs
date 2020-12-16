9c036737e7f21bd0be27229edc7a54dd4337f868
Kiet Tran <tran0563@umn.edu>
2020-12-16 15:24:43 -0600

    diegetic UI for creating visual elements

---
+++ Assets/Scripts/VisualElementInstantiator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisualElementInstantiator : MonoBehaviour
+{
+    public GameObject visualElementPrefab;
+
+    void Awake()
+    {
+        
+    }
+
+    public void InstantiateVisualElement()
+    {
+        GameObject visualElement = Instantiate(visualElementPrefab, transform.position, transform.rotation);
+        visualElement.transform.localScale = transform.localScale * 3f;
+    }
+}
